19f2771a4ca9f7b166db9830c47dba58a3a07a5e
Romain Pelisse <belaran@gmail.com>
2019-03-28 16:11:22 +0100

    whitespaces

--- src/main/java/org/redhat/jboss/webapp/SimpleServlet.java
+++ src/main/java/org/redhat/jboss/webapp/SimpleServlet.java
@@ -1,19 +1,90 @@
 package org.redhat.jboss.webapp;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="simple", urlPatterns="/*")
+@WebServlet(name="info-webapp", urlPatterns="/*")
 public class SimpleServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
+	private static final String TAB = "\t";
+	private static final String PLAIN_TEXT_TYPE = "plain/text";
+	private static final String UTF8_ENC = "UTF-8";
+
+	private static HttpServletResponse configureResponse(HttpServletResponse response) {
+		response.setContentType(PLAIN_TEXT_TYPE);
+		response.setCharacterEncoding(UTF8_ENC);
+		return response;
+	}
+
+	private static String tabs(int nbTabs) {
+		final StringBuffer tabs = new StringBuffer(nbTabs);
+		for ( int i = 0; i < nbTabs ; i++ )
+			tabs.append(TAB);
+		return tabs.toString();
+	}
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-		StringBuffer url = request.getRequestURL();
-		System.out.println(url);
+
+		try {
+			response.getWriter().printf("%s", "hello");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+
+		response = configureResponse(response);
+
+		addToResponse(response, "Requested URL:" + tabs(3) + request.getRequestURL());
+		response.addHeader("JBOSS_NODE_NAME", getLocalHostname());
+		addToResponse(response, "Runs on node:" +  tabs(3) + getLocalHostnameString());
+		response.addHeader("REQUESTER", request.getRemoteHost());
+		addToResponse(response, "Requested by:" + tabs(3) + getRemoteHostString(request) );
+		response.setStatus(HttpServletResponse.SC_OK);
 		return;
+	}
+
+	private static String getRemoteHostString(final HttpServletRequest request) {
+		return request.getRemoteHost() + " [IP: " + request.getRemoteAddr() + ", port: " + request.getRemotePort()  + " ";
+	}
+
+	private static String getLocalHostname()  {
+		try {
+			return InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			throw new IllegalStateException("Can't add retrieve local hostname ! Something is quite wrong server side.");
+		}
+	}
+
+	private static String getLocalHostIP()  {
+		try {
+			return 	InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			throw new IllegalStateException("Can't add retrieve local hostname ! Something is quite wrong server side.");
+		}
+	}
+
+
+	private static String getLocalHostnameString()  {
+			return getLocalHostname() + " [IP: " + getLocalHostIP() + " ]" ;
+	}
+
+	private static HttpServletResponse addToResponse(final HttpServletResponse response, String content) {
+		try {
+			response.getWriter().write(content + "\n");
+		} catch (IOException ioe ) {
+			throw new IllegalStateException("Can't add data to the HTTP response ! Something is quite wrong server side.");
+		}
+		return response;
 	}
 }
