f9ee740906d15ff7b20cd583eaf7b0aa3ce7f963
Romain Pelisse <belaran@gmail.com>
2022-12-20 12:26:06 +0100

    Rework app to works with Jakarta 8

--- src/main/java/org/redhat/jboss/webapp/SimpleServlet.java
+++ src/main/java/org/redhat/jboss/webapp/SimpleServlet.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name="info-webapp", urlPatterns="/*")
 public class SimpleServlet extends HttpServlet {
